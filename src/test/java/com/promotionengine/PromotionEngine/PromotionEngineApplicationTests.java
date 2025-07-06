554a2b611ec0d9ce3cf63c84849d1d3ed89bb82c
sufiyan438 <sufiyanshakil438@gmail.com>
2025-07-06 15:39:41 +0530

    first commit

---
+++ src/test/java/com/promotionengine/PromotionEngine/PromotionEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promotionengine.PromotionEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotionEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
