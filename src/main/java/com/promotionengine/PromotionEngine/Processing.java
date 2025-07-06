ffc290e6500dc7dc45751fd77f4c4f8dc908a1e9
sufiyan438 <sufiyanshakil438@gmail.com>
2025-07-06 18:00:00 +0530

    Forgot to add the output lines in Processing code

--- src/main/java/com/promotionengine/PromotionEngine/Processing.java
+++ src/main/java/com/promotionengine/PromotionEngine/Processing.java
@@ -7,5 +7,9 @@ public class Processing {
         engine.addPromotion(new NItemsPromotion(SKU.A, constants.SKU_A_REQUIRED_QUANTITY, constants.SKU_A_PROMO_PRICE));
         engine.addPromotion(new NItemsPromotion(SKU.B, constants.SKU_B_REQUIRED_QUANTITY, constants.SKU_B_PROMO_PRICE));
         engine.addPromotion(new ComboPromotion(SKU.C, SKU.D, constants.COMBO_PRICE_FOR_C_AND_D));
+
+        BillingService billingService = new BillingService();
+        int total = billingService.generateBill(cart, engine);
+        new OutputPrint().printTotal(total);
     }
 }
