a4258f2c820642af4013cc03a85a455bb87e1bdb
sufiyan438 <sufiyanshakil438@gmail.com>
2025-07-06 16:57:35 +0530

    File parsing code written

--- src/main/java/com/promotionengine/PromotionEngine/PromotionEngineApplication.java
+++ src/main/java/com/promotionengine/PromotionEngine/PromotionEngineApplication.java
@@ -1,13 +1,35 @@
 package com.promotionengine.PromotionEngine;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PromotionEngineApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(PromotionEngineApplication.class, args);
+		if(args.length != 1){
+			System.out.println("Provide the input file path!");
+			return;
+		}
+		Cart cart = new Cart();
+		try(BufferedReader br = new BufferedReader(new FileReader(args[0]))){
+			String line = br.readLine();
+			while(line != null){
+				processCommand(line, cart);
+				line = br.readLine();
+			}
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
+
+	private static void processCommand(String line, Cart cart){
+		String[] parts = line.split(" ");
+		SKU sku = SKU.valueOf(parts[0]);
+		int quantity = Integer.parseInt(parts[1]);
+		cart.addItem(sku, quantity);
 	}
 
 }
