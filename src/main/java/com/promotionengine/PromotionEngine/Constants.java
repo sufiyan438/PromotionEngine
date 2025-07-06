206e8926d9bd73db882223081f5ce98f3825a5f0
sufiyan438 <sufiyanshakil438@gmail.com>
2025-07-06 18:15:33 +0530

    Made all constants as static

--- src/main/java/com/promotionengine/PromotionEngine/Constants.java
+++ src/main/java/com/promotionengine/PromotionEngine/Constants.java
@@ -1,11 +1,11 @@
 package com.promotionengine.PromotionEngine;
 
 public class Constants {
-    public int SKU_A_PROMO_PRICE = 130;
-    public int SKU_B_PROMO_PRICE = 45;
-    public int SKU_A_REQUIRED_QUANTITY = 3;
-    public int SKU_B_REQUIRED_QUANTITY = 2;
-    public int COMBO_PRICE_FOR_C_AND_D = 30;
+    public static int SKU_A_PROMO_PRICE = 130;
+    public static int SKU_B_PROMO_PRICE = 45;
+    public static int SKU_A_REQUIRED_QUANTITY = 3;
+    public static int SKU_B_REQUIRED_QUANTITY = 2;
+    public static int COMBO_PRICE_FOR_C_AND_D = 30;
     public static int A_PRICE = 50;
     public static int B_PRICE = 30;
     public static int C_PRICE = 20;
--- src/main/java/com/promotionengine/PromotionEngine/Processing.java
+++ src/main/java/com/promotionengine/PromotionEngine/Processing.java
@@ -2,11 +2,11 @@ package com.promotionengine.PromotionEngine;
 
 public class Processing {
     public void processingAndOutput(Cart cart){
-        Constants constants = new Constants();
+        // Constants constants = new Constants();
         PromotionEngine engine = new PromotionEngine();
-        engine.addPromotion(new NItemsPromotion(SKU.A, constants.SKU_A_REQUIRED_QUANTITY, constants.SKU_A_PROMO_PRICE));
-        engine.addPromotion(new NItemsPromotion(SKU.B, constants.SKU_B_REQUIRED_QUANTITY, constants.SKU_B_PROMO_PRICE));
-        engine.addPromotion(new ComboPromotion(SKU.C, SKU.D, constants.COMBO_PRICE_FOR_C_AND_D));
+        engine.addPromotion(new NItemsPromotion(SKU.A, Constants.SKU_A_REQUIRED_QUANTITY, Constants.SKU_A_PROMO_PRICE));
+        engine.addPromotion(new NItemsPromotion(SKU.B, Constants.SKU_B_REQUIRED_QUANTITY, Constants.SKU_B_PROMO_PRICE));
+        engine.addPromotion(new ComboPromotion(SKU.C, SKU.D, Constants.COMBO_PRICE_FOR_C_AND_D));
 
         BillingService billingService = new BillingService();
         int total = billingService.generateBill(cart, engine);
