3c24c6c62a3142d4db97b0f6cddd8cce43200948
sufiyan438 <sufiyanshakil438@gmail.com>
2025-07-06 17:15:28 +0530

    N items test case written

--- src/test/java/com/promotionengine/PromotionEngine/PromotionEngineApplicationTests.java
+++ src/test/java/com/promotionengine/PromotionEngine/PromotionEngineApplicationTests.java
@@ -1,13 +1,28 @@
 package com.promotionengine.PromotionEngine;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PromotionEngineApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
+	@Test
+	void testApplyfor4As(){
+		Map<SKU, Integer> cart = new HashMap<>();
+		cart.put(SKU.A, 4);
+		NItemsPromotion promo = new NItemsPromotion(SKU.A, 3, 130);
+		assertTrue(promo.isApplicable(cart));
+		int result = promo.apply(cart);
+		assertEquals(180, result);
+		assertEquals(0, cart.get(SKU.A));
+	}
 }
